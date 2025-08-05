072899eada312eeec40c8bf7765b1df57fa73b10
rajdeep-office <rajdeep.das5@cognizant.com>
2025-08-05 22:04:52 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UploadResume from "./components/UploadResume";
+
+function App() {
+  return (
+    <div style={{ maxWidth: 600, margin: "40px auto", fontFamily: "sans-serif" }}>
+      <h1>Resume Scoring Agent</h1>
+      <UploadResume />
+    </div>
+  );
+}
+
+export default App;
