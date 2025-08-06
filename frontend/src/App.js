1541bc32b6a0c7107f0720f9fe7dd93030006fc2
rajdeep-office <rajdeep.das5@cognizant.com>
2025-08-06 19:02:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import UploadResume from "./components/UploadResume";
 function App() {
   return (
     <div style={{ maxWidth: 600, margin: "40px auto", fontFamily: "sans-serif" }}>
-      // <img src ='resume_scroing.jpeg'/>
+      
   <h1>Resume Scoring Agent</h1>
       <UploadResume />
     </div>
