90993a40054d9f0a77acc8ab4bd0de835386fc83
rajdeep-office <rajdeep.das5@cognizant.com>
2025-08-11 02:50:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import UploadResume from "./components/UploadResume";
 
 function App() {
   return (
-    <div style={{background: "purple", maxWidth: 600, margin: "40px auto", fontFamily: "sans-serif", color:"white" }}>
+    <div style={{background: "purple", maxWidth: "auto", maxHeight: "auto", fontFamily: "sans-serif", color:"white" }}>
       
   <h1> Resume Scoring Agent </h1>
       <UploadResume />
