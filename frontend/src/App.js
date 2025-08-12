3095ee5f611f08628da94210083448093c0e4933
rajdeep-office <rajdeep.das5@cognizant.com>
2025-08-13 00:25:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div
       style={{
-        background: "linear-gradient(to right, #FCFFE9, #FFF2CC)",
+        background: "linear-gradient(to right, #F0F8FF, #00FFFF)",
         minHeight: "100vh",
         padding: "40px",
         fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
